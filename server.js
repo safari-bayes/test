4080113d4614a079fb8152a99c0beb22bab528d2
samson <ssafari@verst.earth>
2026-01-17 01:46:40 +0300

    fixed

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const podName = process.env.HOSTNAME || "unknown-pod";
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end(`Hello from Node.js in Kubernetes!\nServed by pod: ${podName}\n`);
+  res.end(`Hello from Samson in Kubernetes!\nServed by pod: ${podName}\n`);
 });
 
 server.listen(3000, () => {
